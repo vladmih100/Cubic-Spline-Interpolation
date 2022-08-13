080d1f71779751cfe058f7147c070513d8447ed3
vladmih100 <76131954+vladmih100@users.noreply.github.com>
2022-08-14 02:42:47 +1200

    Update sdlab_earthquakes.py

--- sdlab_earthquakes.py
+++ sdlab_earthquakes.py
@@ -1,17 +1,3 @@
-# ENGSCI233: Lab - Sampled Data
-# sdlab_earthquakes.py
-
-# PURPOSE:
-# To INVESTIGATE a dataset using interpolation and integration methods.
-
-# PREPARATION:
-# Complete the activities in sdlab_practice.py.
-
-# SUBMISSION:
-# - YOU MUST submit a plot of NET MASS CHANGE as a function of time (sdlab_earthquakes.png)
-# - You MUST submit this file to complete the lab.
-
-
 # EXERCISE: Analysis of Net Mass Changes.
 #
 # Earthquakes are sometimes associated with oil and gas production (taking mass out of the
@@ -39,20 +25,6 @@
 # - Write a Newton-Cotes integration function OR find and use a built-in Python function.
 # - Produce a plot of NET MASS CHANGE as a function of time.
 # - ANSWER the questions in sdlab_questions.txt
-
-
-# HINTS:
-# - To add or difference two quantities, they should be measured or interpolated at the same time.
-# - You should consider a sensible strategy for the event that an interpolation point lies outside
-#   the range of data (extrapolation).
-# - MASS RATE is a derivative (per unit time) and CUMULATIVE MASS is its integral.
-# - You will be assessed on the ability of your figure to convey information. Things that help:
-#    o Sensible labels (and units) for the axes.
-#    o A legend.
-#    o Sensible use of lines versus markers.
-#    o Juxtaposition of information in a way that highlights correlation.
-# - The plotting exercise in PRACTICE TASK ONE is relevant here.
-
 
 from matplotlib import pyplot as plt  # MATPLOTLIB is THE plotting module for Python
 from numpy.linalg import solve
